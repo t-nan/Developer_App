9731c805cfef7d408bc89f506eabc1ee31f845b3
t-nan <t-nan@yandex.ru>
2021-05-31 18:16:23 +0300

    add flat model

---
+++ app/models/flat.rb
@@ -0,0 +1,16 @@
+class Flat < ApplicationRecord
+
+  has_attached_file :image, :styles => { :medium => "450x450>", :thumb => "50x50>" }
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+  belongs_to :plan
+
+
+
+  validates :number, presence: true, numericality: { only_integer: true},  uniqueness: true
+  validates :floor, presence: true, numericality: { only_integer: true}
+  validates :square, presence: true, numericality: { only_integer: true}
+  validates :entrance, presence: true
+  validates :status, exclusion: [nil]
+  
+end
