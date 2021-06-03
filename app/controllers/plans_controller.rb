82af0cfb9b61c686b5608c4ac8d483e46d9459b0
t-nan <t-nan@yandex.ru>
2021-06-03 14:37:56 +0300

    remove some code from plans controlle

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -17,53 +17,21 @@ class PlansController < ApplicationController
   end
 
   def new
+    render inline: "ERROR"
   end
 
   def create
-
-    @plan=Plan.new(plan_params)
-
-    if @plan.save
-      redirect_to plans_path
-    else
-      render "new"
-    end
-
   end
 
   def edit
-    @plan=Plan.find(params[:id])
-    @flat=@plan.flats
   end
 
   def update
-
-    @plan=Plan.find(params[:id])
-    if @plan.update(plan_params)
-      redirect_to plans_path
-    else
-      render "edit"
-    end
-
   end
 
 
   def destroy
-    @plan=Plan.find(params[:id])
-    @plan.delete
-
-    redirect_to plans_path
+   
   end
-
-
-  private
-
-  def plan_params
-    params.require(:plan).permit(:kind , :rooms , :image )
-  end
-
-
-
-
 
 end
