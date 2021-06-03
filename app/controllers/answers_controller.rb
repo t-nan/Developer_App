a5a53ba08bd95916b0194ab529a436b04d513238
t-nan <t-nan@yandex.ru>
2021-06-03 14:27:03 +0300

    remove some code from answers & question controllers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,38 +1,3 @@
 class AnswersController < ApplicationController
 
-  def index
-  end
-
-  def show
-    @plan=Plan.find(params[:plan_id])
-    @flat=@plan.flats.find(params[:flat_id])
-    @question=@flat.questions.find(params[:question_id]) 
-    @answer=@question.answers.find(params[:id]) 
-  end
-
-  def new
-  end
-
-  def create
-    @plan=Plan.find(params[:plan_id])
-    @question=@plan.questions.find(params[:question_id])
-    @answer=@question.answers.new (answer_params)
-
-    if @answer.save
-      redirect_to plan_question_path(@plan,@question)
-    else
-      render "show"
-    end
-
- 
-
-  end
-
-  
-  private
-
-  def answer_params
-    params.require(:answer).permit(:author,:body)
-  end
-
 end
