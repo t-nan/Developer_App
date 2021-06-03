3ed7ee254302fecc07badd9f0706877c16d716c7
t-nan <t-nan@yandex.ru>
2021-06-03 14:21:50 +0300

    add admin flats controller

---
+++ app/controllers/admin/flats_controller.rb
@@ -0,0 +1,66 @@
+class Admin::FlatsController < Admin::ApplicationController
+
+  def index
+    
+    @flats=Flat.all
+
+  end
+
+  def show
+    @plan=Plan.find(params[:plan_id])
+    @flat=@plan.flats.find(params[:id])
+  end
+
+  def new
+    @plan=Plan.find(params[:plan_id])
+    @flats=@plan.flats
+  end
+
+  def create
+
+    @plan=Plan.find(params[:plan_id])
+
+    @flat=@plan.flats.new(flat_params)
+
+    if @flat.save
+      redirect_to admin_plan_path (@plan)
+    else
+      render "new"
+    end
+
+  end
+
+  def edit
+    @plan=Plan.find(params[:plan_id])
+    @flat=@plan.flats.find(params[:id])
+  end
+
+  def update
+    @plan=Plan.find(params[:plan_id])
+    @flat=@plan.flats.find(params[:id])
+   
+    if @flat.update(flat_params)
+      redirect_to admin_plan_path(@plan)
+    else
+      render "edit"
+    end
+
+  end
+
+
+  def destroy
+    @plan=Plan.find(params[:plan_id])
+    @flat=@plan.flats.find(params[:id])
+    @flat.delete
+
+    redirect_to admin_plan_path (@plan)
+  end
+
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:number,:entrance,:floor,:square,:rooms,:status,:image)
+  end
+
+end
