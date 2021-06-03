cd090a057f343b87e777256fb63135b9268410ea
t-nan <t-nan@yandex.ru>
2021-06-03 14:21:35 +0300

    add admin contacts controller

---
+++ app/controllers/admin/contacts_controller.rb
@@ -0,0 +1,20 @@
+class Admin::ContactsController < Admin::ApplicationController
+
+  def new
+  end
+
+  def create
+    @contact=Contact.new(contact_params)
+     if @contact.save
+      redirect_to flats_path
+    end
+  end
+
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :surname, :phone ,:flat)
+  end
+
+end
