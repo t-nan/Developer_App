63829f5a7de099539da614a57e74c38de06ae115
t-nan <t-nan@yandex.ru>
2021-06-05 22:34:46 +0300

    add validations to plan model

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,5 +1,8 @@
 class Plan < ApplicationRecord
 
+  validates :kind, presence: true, numericality: { only_integer: true},  uniqueness: true
+  validates :rooms, presence: true, numericality: { only_integer: true}
+
   has_attached_file :image, :styles => { :medium => "450x450>", :thumb => "130x130>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   
