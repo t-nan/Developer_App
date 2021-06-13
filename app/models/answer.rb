582853674d1c5e02dd50503a94063b90e47efebe
t-nan <t-nan@yandex.ru>
2021-06-13 18:41:19 +0300

    add length validations to answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,7 @@
 class Answer < ApplicationRecord
+
+  validates :author, presence: true ,length: {maximum: 50}
+  validates :body, presence: true ,length: {maximum: 140}
+
   belongs_to :question
 end
