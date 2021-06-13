aac4b0105dff29c23cfc56d68f546e84619eeeb7
t-nan <t-nan@yandex.ru>
2021-06-13 18:41:48 +0300

    add length validations to contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,8 @@
 class Contact < ApplicationRecord
 
-  validates :name, presence: true 
-  validates :surname, presence: true
-  validates :phone, presence: true, length: {minimum: 7}
+  validates :name, presence: true ,length: {maximum: 50}
+  validates :surname, presence: true ,length: {maximum: 50}
+  validates :phone, presence: true, length: {minimum: 7,maximum: 15}
   validates :flat, presence: true ,numericality: { only_integer: true}
   
 end
