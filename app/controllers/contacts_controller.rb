f100e23511bcbaf00047b4ca010d74cbb70b2623
t-nan <t-nan@yandex.ru>
2021-06-16 13:33:13 +0300

    add messages to contact controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,8 +6,13 @@ class ContactsController < ApplicationController
 
   def create
     @contact=Contact.new(contact_params)
-    @contact.save
-      redirect_to flats_path
+
+    if @contact.save
+       redirect_to flats_path, notice: "Your contact has been saved!"
+    else
+       redirect_to flats_path, alert: "Contact not saved ! Make sure that the forms are filled in correctly."
+    end
+
   end
 
 
