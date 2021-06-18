5e41921512c0bd6a4acacecc4bfa8d60d1e41f51
t-nan <t-nan@yandex.ru>
2021-06-18 19:21:33 +0300

    add flats factory

---
+++ spec/factories/flats.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :flat do
+    plan_id {"1"}
+    number {"1"}
+    entrance {"1"}   
+    floor {"1"} 
+    square {"25"} 
+    rooms {"1"} 
+    status {"true"}
+  end
+end
