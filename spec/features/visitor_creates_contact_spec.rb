459e5f8783b232031a5ba2b39b0884b1f91924bd
t-nan <t-nan@yandex.ru>
2021-06-18 19:36:00 +0300

    add acceptance test visitor creates contact spec

---
+++ spec/features/visitor_creates_contact_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+feature "Contact creation" do
+
+  scenario "allows acess to contacts form" do
+    visit plans_path
+
+    click_button 'To book'
+    expect(page).to have_content 'Your contacts'
+  end
+
+
+  scenario "allows a guest to create contact" do
+    visit plans_path
+
+    click_button 'To book'
+
+    fill_in :contact_name, with: 'Peter'
+    fill_in :contact_surname, with: 'Griffin'
+    fill_in :contact_phone, with: '000000000000'
+    fill_in :contact_flat, with: '1'
+
+    click_button 'Save contact'
+
+    expect(page).to have_content 'Your contact has been saved!'
+  end
+
+  scenario "doesn't allow create a contact if the guest entered letters instead of numberst" do
+    visit plans_path
+
+    click_button 'To book'
+
+    fill_in :contact_name, with: 'Peter'
+    fill_in :contact_surname, with: 'Griffin'
+    fill_in :contact_phone, with: 'abc'
+    fill_in :contact_flat, with: 'xy'
+
+    click_button 'Save contact'
+
+    expect(page).to have_content 'Contact not saved ! Make sure that the forms are filled in correctly.'
+  end
+
+
+end
