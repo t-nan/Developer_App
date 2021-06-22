69794ea428f00858ec673a12005386d5f67d7883
t-nan <t-nan@yandex.ru>
2021-06-23 00:49:22 +0300

    add validation length to question body

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
 
   validates :author, presence: true
-  validates :body, presence: true
+  validates :body, presence: true ,length: {maximum: 200}
 
   belongs_to :plan
   has_many :answers
